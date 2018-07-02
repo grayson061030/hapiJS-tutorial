26ea614aa25fdab0134eb2a590a31849557a1366
GavinKim <dreamworld612@gmail.com>
2018-07-02 23:13:00 +0900

    added dependency and apply authentication into all api dependency: good, good-console,good-squeeze,hapi-auth-jwt2

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const hapi = require('hapi');
 const server = new hapi.Server();
 const plugins = require('./config/plugins');
+const JwtService = require('./services/jwt.service');
 
 server.connection({host:'localhost',port: '3000'});
 
-// root route
-server.route({
-    path: '/',
-    method: 'GET',
-    handler(req,reply) {
-        reply('Welcome to HapiJs Tutorial');
-    }
-});
 
-//plugins [swagger,mongoose]
 server.register(plugins,(err) => {
     if(err) {
         throw err;
@@ -21,7 +13,17 @@ server.register(plugins,(err) => {
     server.start(err=> {
         if (err){
             throw err;
-        }
+        };
+        server.auth.strategy('jwt','jwt',{
+            key: '4@#$5234asd',
+            verifyOptions: {
+                algorithm: ['HS256']
+            },
+            validateFunc: JwtService.validate
+        });
+
+        server.auth.default('jwt');
+
         console.log(`Server Running at ${server.info.port}`);
     });
 });
